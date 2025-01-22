833e6e8dea6b1e6454f34855ad28b20c16372b5f
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-01-22 18:28:04 +0100

    paginas grupos

---
+++ src/app/grupos/[id]/page.jsx
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
+
+const prisma = new PrismaClient()
+
+async function PaginaGrupo({ params }) {
+    const parametros = await params;
+    const grupo = await prisma.grupo.findUnique({where: {id: Number(parametros.id)}})
+    return ( 
+        <div>
+            <Link href="/grupos"><b>GRUPOS</b></Link>
+            <h1>Grupo con Id {grupo.id}</h1>
+            <p>Nombre: {grupo.nombre} </p>
+            <p>Tutor: {grupo.tutor} </p>
+            <p>Aula: {grupo.aula}</p>
+        </div>
+
+     );
+}
+
+export default PaginaGrupo;
---
+++ src/app/grupos/page.jsx
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
+
+const prisma = new PrismaClient();
+
+
+async function PaginaGrupos() {
+    const grupos = await prisma.grupo.findMany()
+  
+
+  return (
+    <div>
+      <Link href="/"><b>HOME</b></Link>
+      {grupos.map((grupo) => (
+        <div className="flex gap-2" key={grupo.id}>
+          <p><b>Nombre: </b>{grupo.nombre}</p>
+          <p><b>Tutor: </b>{grupo.tutor}</p>
+          <p><b>Aula: </b>{grupo.aula}</p>
+          <p><Link href='/grupos/1'>VER</Link></p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PaginaGrupos;
