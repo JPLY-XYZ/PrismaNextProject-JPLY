7db1362be6dcdc0ce23d1c997075cc4956c3d4e8
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-01-22 19:32:25 +0100

    suspense y fallback creados

--- src/app/grupos/[id]/page.jsx
+++ src/app/grupos/[id]/page.jsx
@@ -1,21 +1,52 @@
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
+import { Suspense } from "react";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function PaginaGrupo({ params }) {
-    const {id} = await params;
-    const grupo = await prisma.grupo.findUnique({where: {id: +id}})
-    return ( 
-        <div>
-            <Link href="/grupos"><b>GRUPOS</b></Link>
-            <h1>Grupo con Id {grupo.id}</h1>
-            <p>Nombre: {grupo.nombre} </p>
-            <p>Tutor: {grupo.tutor} </p>
-            <p>Aula: {grupo.aula}</p>
+  return (
+    <Suspense
+      fallback={
+        <div className="animate-pulse">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="300"
+            height="300"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 6.001h18m-18 6h18m-18 6h18"
+            />
+          </svg>
         </div>
-
-     );
+      }
+    >
+      {" "}
+      <Lista params={params} />
+    </Suspense>
+  );
 }
 
 export default PaginaGrupo;
+
+async function Lista({ params }) {
+  const { id } = await params;
+  const grupo = await prisma.grupo.findUnique({ where: { id: +id } });
+  return (
+    <div>
+      <Link href="/grupos">
+        <b>GRUPOS</b>
+      </Link>
+      <h1>Grupo con Id {grupo.id}</h1>
+      <p>Nombre: {grupo.nombre} </p>
+      <p>Tutor: {grupo.tutor} </p>
+      <p>Aula: {grupo.aula}</p>
+    </div>
+  );
+}
--- src/app/grupos/page.jsx
+++ src/app/grupos/page.jsx
@@ -1,27 +1,69 @@
 import { PrismaClient } from "@prisma/client";
 import Link from "next/link";
+import { Suspense } from "react";
 
 const prisma = new PrismaClient();
 
-
 async function PaginaGrupos() {
-    const grupos = await prisma.grupo.findMany()
-  
-    console.log(grupos)
+  return (
+    <Suspense
+      fallback={
+        <div className="animate-pulse">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="300"
+            height="300"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 6.001h18m-18 6h18m-18 6h18"
+            />
+          </svg>
+        </div>
+      }
+    >
+      {" "}
+      <Lista />
+    </Suspense>
+  );
+}
+
+export default PaginaGrupos;
+
+async function Lista() {
+  const grupos = await prisma.grupo.findMany();
+
+  console.log(grupos);
 
   return (
     <div>
-      <Link href="/"><b>HOME</b></Link>
+      <Link href="/">
+        <b>HOME</b>
+      </Link>
       {grupos.map((grupo) => (
         <div className="flex gap-2" key={grupo.id}>
-          <p><b>Nombre: </b>{grupo.nombre}</p>
-          <p><b>Tutor: </b>{grupo.tutor}</p>
-          <p><b>Aula: </b>{grupo.aula}</p>
-          <p><Link href={`/grupos/${grupo.id}`}>VER</Link></p>
+          <p>
+            <b>Nombre: </b>
+            {grupo.nombre}
+          </p>
+          <p>
+            <b>Tutor: </b>
+            {grupo.tutor}
+          </p>
+          <p>
+            <b>Aula: </b>
+            {grupo.aula}
+          </p>
+          <p>
+            <Link href={`/grupos/${grupo.id}`}>VER</Link>
+          </p>
         </div>
       ))}
     </div>
   );
 }
-
-export default PaginaGrupos;
